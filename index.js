db37bc74b1e95f5f3571ade2f59ce2b3755d706c
Hernan Condori <hcondori@elementum.com>
2021-09-20 22:43:54 -0300

    CLASE 5 parte 2

---
+++ index.js
@@ -0,0 +1,15 @@
+const minmaxaverage = require('minmaxaverage');
+const moment = require('moment');
+
+// const average = minmaxaverage.getAverage([10, 5, 15]);
+// console.log(average);
+
+// const min = minmaxaverage.getMinNumber([10, 5, 15]);
+// console.log(min);
+
+// const max = minmaxaverage.getMaxNumber([10, 5, 15]);
+
+// console.log(max)
+
+const today = moment();
+console.log(today);
