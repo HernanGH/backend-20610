4e918bba3fdb4dd791bf8631244b7d52b8651993
Hernan Condori <hcondori@elementum.com>
2021-10-06 13:54:39 -0300

    PUG

---
+++ pug_engine/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/hello', (req, res) => {
+  res.render('hello.pug', { mensaje: 'usando pug JS en express' });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
