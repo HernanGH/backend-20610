717bdc65d8a3f4b5fdfe19f36a7f328abdf5d4ec
Hernan Condori <hcondori@elementum.com>
2021-12-03 19:35:49 -0300

    AFTERCLASS 11  - Sessiones

---
+++ src/routers/web/auth.js
@@ -0,0 +1,44 @@
+import { Router } from 'express'
+
+import path from 'path'
+
+const authWebRouter = new Router()
+
+authWebRouter.get('/', (req, res) => {
+    res.redirect('/home')
+})
+
+authWebRouter.get('/login', (req, res) => {
+    const nombre = req.session?.nombre
+    if (nombre) {
+        res.redirect('/')
+    } else {
+        res.sendFile(path.join(process.cwd(), '/views/login.html'))
+    }
+})
+
+authWebRouter.get('/logout', (req, res) => {
+    const nombre = req.session?.nombre
+    if (nombre) {
+        req.session.destroy(err => {
+            if (!err) {
+                res.render(path.join(process.cwd(), '/views/pages/logout.ejs'), { nombre })
+            } else {
+                res.redirect('/')
+            }
+        })
+    } else {
+        res.redirect('/')
+    }
+})
+
+
+authWebRouter.post('/login', (req, res) => {
+    req.session.nombre = req.body.nombre
+    // si no queres usar motores de plantilla podes devolver el nombre en la url como query params
+    res.redirect('/home?name='+req.body.nombre)
+})
+
+
+
+export default authWebRouter
