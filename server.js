dae917be3dc727ac68708f0d1d864155d972cfd4
Hernan Condori <hcondori@blockchain.com>
2022-01-12 13:23:45 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/message', (req, res) => {
+  res.send('Hola Node.js con Heroku');
+});
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+  console.log(`Server express running in port: ${PORT}`);
+});
+
+server.on('error', (error) => console.log(`Server error ${error}`));
