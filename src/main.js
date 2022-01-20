54d37b52237f338f67aa9560ea4107133d9960f7
Hernan Condori <hcondori@blockchain.com>
2022-01-20 18:40:27 -0300

    - fixes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import env from './env.js'
 import app from './server.js'
 
 const PORT = process.env.PORT || 8080
+
 const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
 })
