d2d1cb044db4b65e5658a71eb357f6b97d95a698
Hernan Condori <hcondori@blockchain.com>
2022-01-21 15:49:12 -0300

    /test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import env from './env.js'
+// import env from './env.js'
 // import app from './server.js'
 import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => res.send('hello world'));
+
+app.get('/test', (req, res) => res.send('[test] - hello world'));
 
 const PORT = process.env.PORT || 8080
 
